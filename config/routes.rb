f4e645f344904d843c7d2312e5bf856cc6621847
Mykyta Troianov <nicktroyanov@gmail.com>
2016-08-04 10:06:53 +0300

    crud for rooms, added pry-debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :rooms
   devise_for :users
+
+  root to: 'rooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
